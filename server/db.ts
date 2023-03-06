9111a999af3bc0e63b13b7874fa3ca12b56691b9
nicsylvia <nicsylvia15f@gmail.com>
2023-03-05 17:48:12 -0800

    connected db

--- server/db.ts
+++ server/db.ts
@@ -4,8 +4,9 @@ const agendastring = "mongodb://localhost/AgendaTutorial";
 
 export const DBconnect = async() =>{
     try {
-        
+        const connectstring = await mongoose.connect(agendastring);
+        console.log(`DB is connected to ${DBconnect}`)
     } catch (error) {
-        return 
+        console.log(`An error occured in connecting DB to ${agendastring} `)
     }
 }
--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,17 @@
 import express, { Application, Request, Response} from "express";
 
 import cors from "cors";
+import { DBconnect } from "./db";
 
 const port: number = 4000;
 
 const app: Application = express();
+DBconnect()
 
 app.use(express.json());
 app.use(cors());
 
 app.listen(port, () =>{
+    console.log("")
     console.log("Listening to server on port", port)
 })
