bd1d2cd8bc01d1ba268806cf9905745630769ebe
nicsylvia <nicsylvia15f@gmail.com>
2023-03-05 17:50:27 -0800

    saved all

--- server/db.ts
+++ server/db.ts
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
-const agendastring = "mongodb://localhost/AgendaTutorial";
+const agendastring = "mongodb://localhost/AgendaTutorial"
 
 export const DBconnect = async() =>{
     try {
         const connectstring = await mongoose.connect(agendastring);
-        console.log(`DB is connected to ${DBconnect}`)
+        console.log(`DB is connected to ${connectstring.connection.host}`)
     } catch (error) {
         console.log(`An error occured in connecting DB to ${agendastring} `)
     }
