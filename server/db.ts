6cdfae1713e646295c33d84a091c07dd9ff38407
nicsylvia <nicsylvia15f@gmail.com>
2023-03-05 17:46:16 -0800

    saved all

---
+++ server/db.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const agendastring = "mongodb://localhost/AgendaTutorial";
+
+export const DBconnect = async() =>{
+    try {
+        
+    } catch (error) {
+        return 
+    }
+}
