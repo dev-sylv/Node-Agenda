f64eca348cf102dc375c333648565ab1f0293ce9
nicsylvia <nicsylvia15f@gmail.com>
2023-03-05 20:29:35 -0800

    saved all

--- server/Controllers/userControllers.ts
+++ server/Controllers/userControllers.ts
@@ -2,8 +2,44 @@ import { Job } from "agenda";
 import { Request, Response } from "express";
 
 import { agenda } from "../Config/db";
+import { environmentVariables } from "../Config/environmentVariables";
+
+import nodemailer from "nodemailer"
 
 import userModels from "../Models/usermodels";
+
+
+// Welcome mail for all users:
+function WelcomeMail(Job, done){
+    
+ const SendWelcomeMail = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: environmentVariables.adminemail,
+        pass: environmentVariables.adminpassword
+    }
+});
+
+let recipentEmail = "recipient@gmail.com"
+
+// Create an email message:
+const MailMessage = {
+    from: "nicsylvia15f@gmail.com",
+    to: recipentEmail,
+    subject: "Welcome to my platform",
+    text: 'Thank you for signing up for our site. We look forward to having you as a member!'
+};
+
+// Send the mail:
+SendWelcomeMail.sendMail(MailMessage, function(error, info){
+    if (error) {
+        console.log(error)
+    } else {
+        console.log("Emil sent to: " + info.response)
+    }
+})
+}
+
 
 // Register a user and send them welcome emails:
 export const UserRegistration = async(req: Request, res: Response): Promise<Response> =>{
