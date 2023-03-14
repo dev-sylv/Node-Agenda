00dde02888f2577fd9d58c2678dbaf0639854509
nicsylvia <nicsylvia15f@gmail.com>
2023-03-14 00:55:38 -0700

    updates

--- client/src/Components/Auth/MainRegister.tsx
+++ client/src/Components/Auth/MainRegister.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import logo from "../Assets/relogo.webp";
+import house from "../Assets/sylvia7.jpg";
 
 const MainRegister = () => {
   return (
@@ -39,5 +40,11 @@ const MainAuth = styled.div`
   width: 100%;
   height: 85vh;
   background-color: blue;
+  /* background-image: url(${house});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: contain; */
 `;
-const Wrap = styled.div``;
+const Wrap = styled.div`
+  background-color: green;
+`;
