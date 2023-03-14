7c314fc79a367db4cb126be676f6a1b14bab71ec
nicsylvia <nicsylvia15f@gmail.com>
2023-03-14 16:41:43 -0700

    saved all

--- client/src/Components/Auth/AdminSignUp.tsx
+++ client/src/Components/Auth/AdminSignUp.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import styled from "styled-components";
+import admin from "../Assets/pexels-rodnae-productions-8293740.jpg";
 
 const AdminSignUp = () => {
   return (
     <div>
       <Container>
         <Logo></Logo>
-        <MainSign></MainSign>
+        <MainSign>
+          <Form></Form>
+          <Image>{/* <img src={admin} alt="" /> */}</Image>
+        </MainSign>
         <Empty></Empty>
       </Container>
     </div>
@@ -19,19 +23,39 @@ const Container = styled.div`
   width: 100%;
   height: 100vh;
   background-color: #00afff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 const Logo = styled.div`
-  width: 100%;
+  width: 10%;
   height: 100vh;
   background-color: #00afff;
 `;
 const MainSign = styled.div`
-  width: 100%;
+  width: 80%;
+  height: 100vh;
+  background-color: red;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+const Empty = styled.div`
+  width: 10%;
   height: 100vh;
   background-color: #00afff;
 `;
-const Empty = styled.div`
-  width: 100%;
-  height: 100vh;
-  background-color: #00afff;
+const Form = styled.div`
+  width: 45%;
+  height: 100%;
+  background-color: pink;
+`;
+const Image = styled.div`
+  width: 40%;
+  height: 100%;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 `;
