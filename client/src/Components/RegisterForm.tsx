e174815d8a4356b3da8a786a25d4d5402da2223f
nicsylvia <nicsylvia15f@gmail.com>
2023-03-15 23:18:59 -0700

    testing

--- client/src/Components/RegisterForm.tsx
+++ client/src/Components/RegisterForm.tsx
@@ -1,7 +1,73 @@
 import React from "react";
 
 const RegisterForm = () => {
-  return <div>RegisterForm</div>;
+  return <div>
+    <Right>
+                <Input type="text" placeholder="Name" />
+                <Input type="email" placeholder="Email" />
+                <Input type="text" placeholder="Subject" />
+                <Box placeholder="Message" />
+                <Button type="submit">Submit</Button>
+              </Right>
+  </div>;
 };
 
 export default RegisterForm;
+
+onst Button = styled.button`
+  height: 50px;
+  width: 150px;
+  background-color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  cursor: pointer;
+  transition: all 350ms;
+  font-weight: bold;
+  margin-top: 15px;
+  color: white;
+  :hover {
+    transform: scale(0.9);
+    background-color: #f5f5f5;
+    color: blue;
+    border: 1px solid purple;
+  }
+`;
+const Box = styled.textarea`
+  /* width: 200px; */
+  display: flex;
+  padding-left: 15px;
+  padding-top: 20px;
+  height: 90px;
+  border: 1px solid #ced4da;
+  outline: none;
+  resize: none;
+  ::placeholder {
+    font-size: 17px;
+  }
+`;
+const Input = styled.input`
+  /* width: 100%; */
+  height: 45px;
+  display: flex;
+  align-items: center;
+  padding-left: 15px;
+  border: 1px solid #ced4da;
+  margin-bottom: 10px;
+  ::placeholder {
+    font-size: 17px;
+  }
+`;
+const Right = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  /* align-items: center;
+    justify-content: center; */
+  background-color: red;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    margin-top: 15px;
+  }
+`;
