8280f4876286662b26e192df9b9dbda86a452875
nicsylvia <nicsylvia15f@gmail.com>
2023-03-19 01:16:56 -0700

    kora pay

--- client/src/Components/KoraPay/Payment.tsx
+++ client/src/Components/KoraPay/Payment.tsx
@@ -1,7 +1,44 @@
 import React from "react";
+import { useKorapay, KorapayButton } from "react-korapay";
 
-const Payment = () => {
-  return <div>Payment</div>;
+const config = {
+  public_key: "pk_test_**your_public_key**",
+  amount: 2000,
+  customer: {
+    name: "John doe",
+    email: "johndoe@gmail.com",
+  },
+  narration: "Testing korapay",
 };
 
-export default Payment;
+export default function App() {
+  const korapayBtnConfig = {
+    ...config,
+    onClose: () => {},
+    onSuccess: () => {},
+    text: "Pay with korapay!",
+  };
+
+  const handleKorapay = useKorapay(config);
+
+  return (
+    <div className="App">
+      <h1>React Wrapper for Korapay collections</h1>
+      <h2>Check it out!</h2>
+
+      <div>
+        <button
+          onClick={() =>
+            handleKorapay({
+              onClose: () => {},
+              onSuccess: () => {},
+            })
+          }>
+          Pay now!
+        </button>
+
+        <KorapayButton {...korapayBtnConfig} />
+      </div>
+    </div>
+  );
+}
